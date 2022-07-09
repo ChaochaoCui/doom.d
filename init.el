7b3100534a77d2f5bf312763988c0c4248cfb333
Chaochao Cui <cuichaochao916@gmail.com>
2022-07-09 11:56:13 +0800

    Update init.el

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
        (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons +prescient)             ; a search engine for love and life
+       ;;(ivy +icons +prescient)             ; a search engine for love and life
        (vertico +icons)          ; the search engine of the future
 
        :ui
